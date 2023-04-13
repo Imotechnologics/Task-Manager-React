1ab2105f0d9c13aded6d7bb66fcef3c85cffca1a
Imotechnologics <imotechnologics@gmail.com>
2023-04-12 18:20:12 -0600

    my_react_app_example

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "My First Task",
+    description: "Description about my first Task",
+  },
+  {
+    id: 1,
+    title: "My Second Task",
+    description: "Description about my Second Task",
+  },
+  {
+    id: 2,
+    title: "My Third Task",
+    description: "Description about my Third Task",
+  },
+];
